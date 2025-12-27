13799409617e89887412c1595eef19756314d597
Shaurya926 <santoshdubey7089@gmail.com>
2025-12-28 00:43:01 +0530

    Beginning_progrma's

--- OOPM_lecture_3rd_sem/Beginning_of_OOPM/2nd_code.cpp
+++ OOPM_lecture_3rd_sem/Beginning_of_OOPM/2nd_code.cpp
@@ -1,6 +1,26 @@
 #include<iostream>
 using namespace std;
-void main()
-{
-    
+
+// Base class
+class Animal {
+public:
+    void speak() {
+        cout << "The animal makes a sound." << endl;
+    }
+};
+// Derived class
+class Dog : public Animal {
+public:
+    void speak() {
+        cout << "The dog barks." << endl;
+    }
+};
+int main() {
+    Animal animal;
+    Dog dog;
+
+    animal.speak(); // Calls base class method
+    dog.speak();    // Calls derived class method
+
+    return 0;
 }
